1be6278cbf431d2499ca0c6679cc2e09b8771afd
Anton Tsivarev <a.tsivarev@corp.vk.com>
2018-11-22 15:25:13 +0300

    Add TF

--- src/App.js
+++ src/App.js
@@ -1,46 +1,142 @@
 import React from 'react';
-import connect from '@vkontakte/vkui-connect';
-import { View } from '@vkontakte/vkui';
+import * as tf from '@tensorflow/tfjs';
+import {Cell, FormLayout, Group, InfoRow, Input, List, Panel, PanelHeader, View} from '@vkontakte/vkui';
 import '@vkontakte/vkui/dist/vkui.css';
 
-import Home from './panels/Home';
-import Persik from './panels/Persik';
+import * as vc from './vocab.json';
+
+const ROUND = 1000;
 
 class App extends React.Component {
-	constructor(props) {
-		super(props);
+    constructor() {
+        super();
 
-		this.state = {
-			activePanel: 'home',
-			fetchedUser: null,
-		};
-	}
+        this.loadModel();
 
-	componentDidMount() {
-		connect.subscribe((e) => {
-			switch (e.detail.type) {
-				case 'VKWebAppGetUserInfoResult':
-					this.setState({ fetchedUser: e.detail.data });
-					break;
-				default:
-					console.log(e.detail.type);
-			}
-		});
-		connect.send('VKWebAppGetUserInfo', {});
-	}
+        this.state = {
+            data: [
+                0, //toxic
+                0, //severe toxic
+                0, //obscene
+                0, //threat
+                0, //insult
+                0  //identity hate
+            ],
+        };
 
-	go = (e) => {
-		this.setState({ activePanel: e.currentTarget.dataset.to })
-	};
+        this.loadModel = this.loadModel.bind(this);
+        this.changeText = this.changeText.bind(this);
+        this.calculate = this.calculate.bind(this);
 
-	render() {
-		return (
-			<View activePanel={this.state.activePanel}>
-				<Home id="home" fetchedUser={this.state.fetchedUser} go={this.go} />
-				<Persik id="persik" go={this.go} />
-			</View>
-		);
-	}
+
+        this.loadModel();
+
+    }
+
+    async calculate(text) {
+        let data = [
+            0, //toxic
+            0, //severe toxic
+            0, //obscene
+            0, //threat
+            0, //insult
+            0  //identity hate
+        ];
+
+        if (text && this.state.model) {
+            let tensorBuffer = tf.zeros([1, 100]).buffer();
+
+            let parts = text.split(' ');
+            let tIndex = 0;
+            parts.forEach(function (value) {
+                if (vc.hasOwnProperty(value)) {
+                    tensorBuffer.set(vc[value], 0, tIndex);
+                    tIndex++;
+                }
+            });
+
+            data = await this.state.model.predict(tensorBuffer.toTensor()).data();
+
+        }
+
+        this.setState({data: data});
+
+    }
+
+    changeText(e) {
+        this.calculate(e.target.value);
+    }
+
+    async loadModel() {
+        let loadedModel = await tf.loadModel('/model.json');
+        this.setState({model: loadedModel});
+    }
+
+    renderScore(score) {
+        return (Math.round(score * 1000) / 10)  + '%';
+    }
+
+    renderEmoji(score) {
+        if (score >= 0.9) {
+            return '🤬';
+        } else if (score >= 0.5) {
+            return '😡';
+        } else if (score >= 0.2) {
+            return '🤭';
+        } else {
+            return '😀'
+        }
+    }
+
+    render() {
+        return (
+            <View activePanel="mainPanel">
+                <Panel id="mainPanel">
+                    <PanelHeader>Toxic detector</PanelHeader>
+                    <Group>
+                        <FormLayout>
+                            <Input type="text" top="Your text" onChange={this.changeText}/>
+                        </FormLayout>
+                    </Group>
+
+                    <Group title="Results">
+                        <List>
+                            <Cell>
+                                <InfoRow title="Toxic">
+                                    {this.renderScore(this.state.data[0])} {this.renderEmoji(this.state.data[0])}
+                                </InfoRow>
+                            </Cell>
+                            <Cell>
+                                <InfoRow title="Severe Toxic">
+                                    {this.renderScore(this.state.data[1])} {this.renderEmoji(this.state.data[1])}
+                                </InfoRow>
+                            </Cell>
+                            <Cell>
+                                <InfoRow title="Obscene">
+                                    {this.renderScore(this.state.data[2])} {this.renderEmoji(this.state.data[2])}
+                                </InfoRow>
+                            </Cell>
+                            <Cell>
+                                <InfoRow title="Threat">
+                                    {this.renderScore(this.state.data[3])} {this.renderEmoji(this.state.data[3])}
+                                </InfoRow>
+                            </Cell>
+                            <Cell>
+                                <InfoRow title="Insult">
+                                    {this.renderScore(this.state.data[4])} {this.renderEmoji(this.state.data[4])}
+                                </InfoRow>
+                            </Cell>
+                            <Cell>
+                                <InfoRow title="Identity Hate">
+                                    {this.renderScore(this.state.data[5])} {this.renderEmoji(this.state.data[5])}
+                                </InfoRow>
+                            </Cell>
+                        </List>
+                    </Group>
+                </Panel>
+            </View>
+        );
+    }
 }
 
 export default App;
